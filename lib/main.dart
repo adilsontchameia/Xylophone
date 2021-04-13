a207655730118f06828f8ea2f2b91da664dbd15f
adilsonchameia <adilsonkchameia@outlook.com>
2021-04-13 16:45:16 +0100

    criacao de varios botoes das notas

--- lib/main.dart
+++ lib/main.dart
@@ -9,18 +9,69 @@ void main() {
 }
 
 class Home extends StatelessWidget {
+  void reproduzir() {
+    final player = AudioCache();
+    player.play("note1.wav");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: FlatButton(
-            onPressed: () {
-              final player = AudioCache();
-              player.play("note1.wav");
-            },
-            child: Text("Reproduzir"),
-          ),
+        child: Column(
+          children: [
+            FlatButton(
+              onPressed: () {
+                reproduzir();
+              },
+              child: Text(""),
+              color: Colors.red,
+            ),
+            FlatButton(
+              onPressed: () {
+                reproduzir();
+              },
+              child: Text(""),
+              color: Colors.yellow,
+            ),
+            FlatButton(
+              onPressed: () {
+                reproduzir();
+              },
+              child: Text(""),
+              color: Colors.purple,
+            ),
+            FlatButton(
+              onPressed: () {
+                reproduzir();
+              },
+              color: Colors.green,
+            ),
+            FlatButton(
+              onPressed: () {
+                reproduzir();
+              },
+              color: Colors.blue,
+            ),
+            FlatButton(
+              onPressed: () {
+                reproduzir();
+              },
+              color: Colors.orange,
+            ),
+            FlatButton(
+              onPressed: () {
+                reproduzir();
+              },
+              color: Colors.cyan,
+            ),
+            FlatButton(
+              onPressed: () {
+                reproduzir();
+              },
+              color: Colors.pink,
+            ),
+          ],
         ),
       ),
     );
